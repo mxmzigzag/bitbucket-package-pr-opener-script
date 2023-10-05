3c4b0ae5c0d0b98fd339db75891883a6911a513c
mxmzigzag <qzzik97@gmail.com>
2023-10-05 17:56:05 +0300

    4 out of 4

--- script.js
+++ script.js
@@ -2,31 +2,21 @@ import axios from "axios";
 
 // input parameters
 const PACKAGE_NAME = "red-trial";
-const PACKAGE_VERSION = "1.0.2";
+const PACKAGE_VERSION = "1.0.25";
 const REPO_SLUG = "pr-opener";
 const WORKSPACE = "mxmzigzag";
-const PROJECT_KEY = "RDT";
 const BRANCH_NAME = `update-package-${new Date().getTime()}`;
-
-// bitbucket credentials (hardcoded)
-const USERNAME = "max_zahorskyi";
-const PASSWORD = "ATBBM7KM8z8heEnavLRNBhTRxfmb62D5E0F1";
 
 // bitbucket API endpoint URL
 const API_URL = `https://api.bitbucket.org/2.0/repositories/${WORKSPACE}/${REPO_SLUG}`;
 
+// bitbucket access token
 const ACCESS_TOKEN =
   "Bearer ATCTT3xFfGN02wvifXjmkTWhGa5VdJuU2r8y-zJd1YEUG8PqIU-XSkXYUtTpfhKxCAqDaBH_Tt5UDOJp3c1qMHl741kPgtfLZMuUCAB7aMuXC35MQqMDFels1DmdTW74UPxpgGcQzudsgY_0jdhWi47NcO4oZyGWfT7GfP-98uO1Klxg2aWN9go=42A7F462";
 
 // headers for requests
-const headers = {
-  "Content-Type": "application/json",
-};
-
-// auth creds
-const auth = {
-  username: USERNAME,
-  password: PASSWORD,
+const AUTH_HEADER = {
+  Authorization: ACCESS_TOKEN,
 };
 
 async function start() {
@@ -43,7 +33,7 @@ async function start() {
     console.log("creating the branch...");
     await axios.post(`${API_URL}/refs/branches`, branchData, {
       headers: {
-        Authorization: ACCESS_TOKEN,
+        ...AUTH_HEADER,
         Accept: "application/json",
       },
     });
@@ -60,8 +50,9 @@ async function readAndUpdatePackage() {
   try {
     console.log("retrieving the package file...");
     const packageResp = await axios.get(`${API_URL}/src/master/package.json`, {
-      auth,
-      headers,
+      headers: {
+        ...AUTH_HEADER,
+      },
     });
     console.log("got it!");
 
@@ -79,7 +70,7 @@ async function readAndUpdatePackage() {
       },
       {
         headers: {
-          Authorization: ACCESS_TOKEN,
+          ...AUTH_HEADER,
           "Content-Type": "application/x-www-form-urlencoded",
         },
       }
@@ -102,27 +93,23 @@ async function createPullRequest() {
       title: `Update ${PACKAGE_NAME} to ${PACKAGE_VERSION}`,
       description: "We updated this package for you!",
       source: {
-        branch: BRANCH_NAME,
-        repository: { full_name: `${PROJECT_KEY}/${REPO_SLUG}` },
+        branch: { name: BRANCH_NAME },
+        repository: { full_name: `${WORKSPACE}/${REPO_SLUG}` },
       },
       destination: {
-        branch: "master",
-        repository: { full_name: `${PROJECT_KEY}/${REPO_SLUG}` },
+        branch: { name: "master" },
+        repository: { full_name: `${WORKSPACE}/${REPO_SLUG}` },
       },
     };
 
     console.log("creating the PR...");
-    await axios.post(
-      `${API_URL}/pullrequests`,
-      {
-        headers: {
-          Authorization: ACCESS_TOKEN,
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
+    await axios.post(`${API_URL}/pullrequests`, pullRequestBody, {
+      headers: {
+        ...AUTH_HEADER,
+        Accept: "application/json",
+        "Content-Type": "application/json",
       },
-      pullRequestBody
-    );
+    });
     console.log("Pull request created successfully.");
   } catch (error) {
     // TODO: Add error handling
