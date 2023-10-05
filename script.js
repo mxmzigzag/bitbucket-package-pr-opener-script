9af7e24f2b4de81492765bce58904836a7693bda
mxmzigzag <qzzik97@gmail.com>
2023-10-05 17:27:18 +0300

    3 out of 4 for now

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const PACKAGE_VERSION = "1.0.2";
 const REPO_SLUG = "pr-opener";
 const WORKSPACE = "mxmzigzag";
 const PROJECT_KEY = "RDT";
-const BRANCH_NAME = "update-package";
+const BRANCH_NAME = `update-package-${new Date().getTime()}`;
 
 // bitbucket credentials (hardcoded)
 const USERNAME = "max_zahorskyi";
@@ -14,6 +14,9 @@ const PASSWORD = "ATBBM7KM8z8heEnavLRNBhTRxfmb62D5E0F1";
 
 // bitbucket API endpoint URL
 const API_URL = `https://api.bitbucket.org/2.0/repositories/${WORKSPACE}/${REPO_SLUG}`;
+
+const ACCESS_TOKEN =
+  "Bearer ATCTT3xFfGN02wvifXjmkTWhGa5VdJuU2r8y-zJd1YEUG8PqIU-XSkXYUtTpfhKxCAqDaBH_Tt5UDOJp3c1qMHl741kPgtfLZMuUCAB7aMuXC35MQqMDFels1DmdTW74UPxpgGcQzudsgY_0jdhWi47NcO4oZyGWfT7GfP-98uO1Klxg2aWN9go=42A7F462";
 
 // headers for requests
 const headers = {
@@ -26,8 +29,8 @@ const auth = {
   password: PASSWORD,
 };
 
-// create a branch
 async function start() {
+  // create a branch
   try {
     // branch payload
     const branchData = {
@@ -37,11 +40,14 @@ async function start() {
       },
     };
 
+    console.log("creating the branch...");
     await axios.post(`${API_URL}/refs/branches`, branchData, {
-      auth,
-      headers,
+      headers: {
+        Authorization: ACCESS_TOKEN,
+        Accept: "application/json",
+      },
     });
-    console.log(`${BRANCH_NAME} branch was created`);
+    console.log(`${BRANCH_NAME} branch was created!`);
     await readAndUpdatePackage();
   } catch (error) {
     // TODO: Add error handling
@@ -52,29 +58,31 @@ async function start() {
 // read file and push new package to the bitbucket branch
 async function readAndUpdatePackage() {
   try {
+    console.log("retrieving the package file...");
     const packageResp = await axios.get(`${API_URL}/src/master/package.json`, {
       auth,
       headers,
     });
+    console.log("got it!");
 
     // create a new package (update what we need)
-    const newPackage = {
-      ...packageResp.data,
-      dependencies: {
-        ...packageResp.data.dependencies,
-        [PACKAGE_NAME]: PACKAGE_VERSION,
-      },
-    };
-    console.log("Package updated");
+    const newPackage = packageResp.data;
+    newPackage.dependencies[PACKAGE_NAME] = PACKAGE_VERSION;
 
-    await axios.put(
-      `${API_URL}/src/${BRANCH_NAME}/package.json`,
+    console.log("updated and sending it to the branch...");
+    await axios.post(
+      `${API_URL}/src`,
       {
         message: `Update ${PACKAGE_NAME} to ${PACKAGE_VERSION}`,
         branch: BRANCH_NAME,
-        content: JSON.stringify(newPackage, null, 2),
+        "package.json": JSON.stringify(newPackage, null, 2),
       },
-      { auth, headers }
+      {
+        headers: {
+          Authorization: ACCESS_TOKEN,
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      }
     );
     console.log(
       `${PACKAGE_NAME} updated to ${PACKAGE_VERSION} in package.json successfully. Check the ${BRANCH_NAME} branch`
@@ -83,27 +91,37 @@ async function readAndUpdatePackage() {
     await createPullRequest();
   } catch (error) {
     // TODO: Add error handling
-    console.log("GET FILE ERR:", error);
+    console.log("GET FILE ERR:", error.response.data);
   }
 }
 
 // create a pull request
 async function createPullRequest() {
   try {
+    const pullRequestBody = {
+      title: `Update ${PACKAGE_NAME} to ${PACKAGE_VERSION}`,
+      description: "We updated this package for you!",
+      source: {
+        branch: BRANCH_NAME,
+        repository: { full_name: `${PROJECT_KEY}/${REPO_SLUG}` },
+      },
+      destination: {
+        branch: "master",
+        repository: { full_name: `${PROJECT_KEY}/${REPO_SLUG}` },
+      },
+    };
+
+    console.log("creating the PR...");
     await axios.post(
       `${API_URL}/pullrequests`,
       {
-        title: `Update ${PACKAGE_NAME} to ${PACKAGE_VERSION}`,
-        source: {
-          branch: BRANCH_NAME,
-          repository: { full_name: `${PROJECT_KEY}/${REPO_SLUG}` },
-        },
-        destination: {
-          branch: "master",
-          repository: { full_name: `${PROJECT_KEY}/${REPO_SLUG}` },
+        headers: {
+          Authorization: ACCESS_TOKEN,
+          Accept: "application/json",
+          "Content-Type": "application/json",
         },
       },
-      { auth, headers }
+      pullRequestBody
     );
     console.log("Pull request created successfully.");
   } catch (error) {
@@ -112,5 +130,5 @@ async function createPullRequest() {
   }
 }
 
-// magic
+// start
 start();
